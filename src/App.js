73a7d07be4f784c551c08d0de7f519a5c3975790
sonthicharoen47 <sonthicharoen47@gmail.com>
2021-03-02 02:27:09 +0700

    clear app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages";
-import ContactPage from "./pages/contact";
-import AboutPage from "./pages/about";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" component={Home} exact />
-        <Route path="/contact" component={ContactPage} exact />
-        <Route path="/about" component={AboutPage} exact />
       </Switch>
     </Router>
   );
